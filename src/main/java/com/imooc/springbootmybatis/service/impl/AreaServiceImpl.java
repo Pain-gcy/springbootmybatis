451f47cb25104e85bbd2a924d23443be5e0a1728
guochunyuan@gitv.cn <guochunyuan@gitv.cn>
2018-05-15 14:04:54 +0800

    complete

--- src/main/java/com/imooc/springbootmybatis/service/impl/AreaServiceImpl.java
+++ src/main/java/com/imooc/springbootmybatis/service/impl/AreaServiceImpl.java
@@ -28,7 +28,6 @@ public class AreaServiceImpl implements AreaService {
 
     @Override
     public Area queryAreaById(int id) {
-        int a = 1/0;
         return areaDao.queryAreaById(id);
     }
 
