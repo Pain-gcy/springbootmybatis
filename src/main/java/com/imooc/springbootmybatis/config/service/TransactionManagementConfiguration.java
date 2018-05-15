0c882c6165c4b5eddd38f006e186e6724118d241
guochunyuan@gitv.cn <guochunyuan@gitv.cn>
2018-05-15 10:00:04 +0800

    添加配置

--- src/main/java/com/imooc/springbootmybatis/config/service/TransactionManagementConfiguration.java
+++ src/main/java/com/imooc/springbootmybatis/config/service/TransactionManagementConfiguration.java
@@ -1,7 +1,13 @@
 package com.imooc.springbootmybatis.config.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
 
 /**
  * @author E470
@@ -9,5 +15,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @Configuration
 @EnableTransactionManagement
-public class TransactionManagementConfiguration {
+public class TransactionManagementConfiguration implements TransactionManagementConfigurer{
+    @Autowired
+    private DataSource dataSource;
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
 }
